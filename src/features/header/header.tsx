b198140f6926744001181c9ebb3e705d8ded791f
mfkfawas <mfkfawas@gmail.com>
2025-01-04 05:42:04 +0300

    css reset

---
+++ src/features/header/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header
+      className="bg-cover bg-no-repeat bg-center h-[100vh]"
+      style={{ backgroundImage: "url(./assets/imgs/header-bg.png)" }}
+    >
+      <div className="h-64 bg-custom-gradient">jiberlekks</div>
+    </header>
+  );
+}
