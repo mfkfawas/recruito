86193af7d9772e164b3c0c0b59c749c587233a93
mfkfawas <mfkfawas@gmail.com>
2025-01-04 03:58:12 +0300

    initialized- fonts-setup

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { lazy, Suspense } from "react";
+import "./App.css";
+const VideoPlaySvg = lazy(() => import("./assets/svgs/video-play"));
+import CanImg from "./assets/imgs/canidate-sourcing-graph.png";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline text-red-500">test</h2>
+      <div className="grid place-items-center">
+        <Suspense fallback={<h1 className="text-9xl">Loading....</h1>}>
+          <div className="relative">
+            <VideoPlaySvg />
+            <img
+              src={CanImg}
+              alt="candidate-sourcing-graph"
+              loading="lazy"
+              className="absolute top-1/2 -translate-y-1/2 left-0 w-[21rem] h-[21rem]"
+            />
+          </div>
+        </Suspense>
+      </div>
+    </>
+  );
+}
+
+export default App;
